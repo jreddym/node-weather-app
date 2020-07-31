bed0203d983e1891eeaef08c8d9a652b5e8cec5f
Jagadeeswarareddy M <maramreddy95@gmail.com>
2020-07-31 21:36:06 +0530

    Adding changes to deploy on Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,14 @@ weatherFormElem.addEventListener('submit', (e) => {
   const location = searchElem.value;
   messageOne.textContent = 'Loading .....';
   messageTwo.textContent = '';
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.forecast;
-          messageTwo.textContent = data.location;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.forecast;
+        messageTwo.textContent = data.location;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geoCode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -104,25 +105,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server started in PORT : 3000');
+app.listen(port, () => {
+  console.log(`Server started in PORT : ${port}`);
 });
-
-// geoCode(address, (error, { longitude, latitude, location } = {}) => {
-//   if (error) {
-//     return res.send({
-//       error,
-//     });
-//   }
-//   forecast(latitude, longitude, (error, forecastData) => {
-//     if (error) {
-//       return res.send({
-//         error,
-//       });
-//     }
-//     res.send({
-//       location,
-//       forecast: forecastData,
-//     });
-//   });
-// });
